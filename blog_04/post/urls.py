2e32987090909f32101030aaba3fbec8933f370c
huhaiqiang <you@example.com>
2020-07-25 00:07:42 +0800

    更新4、5代码

---
+++ blog_04/post/urls.py
@@ -0,0 +1,11 @@
+#coding=utf-8
+
+from django.conf.urls import url
+
+from post import views
+
+urlpatterns=[
+    url(r'^$',views.queryAll),
+    url(r'^page/(\d+)$',views.queryAll),
+    url(r'^post/(\d+)$',views.postDetail)
+]
---
+++ blog_05/post/templatetags/myfilter.py
@@ -0,0 +1,11 @@
+# coding=utf-8
+
+from django.template import Library
+
+register = Library()
+
+
+@register.filter
+def md(value):
+    import markdown
+    return markdown.markdown(value)
